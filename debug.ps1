2e212a45aea21a1d2a0b12808b35cf7f2c085d87
Michael Mayer <mjmayer@gmail.com>
2018-11-05 13:17:53 -0800

    Rename files

--- debug.ps1
+++ debug.ps1
@@ -1,4 +1,4 @@
-Import-Module .\yubikey.psm1 -Force
+Import-Module .\VBoxYubikey.psm1 -Force
 $Env:VBoxManage = 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe'
 Connect-yubikey
 Disconnect-Yubikey
